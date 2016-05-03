b483475816138de3acc736c1540fd22472bef910
huangyuan <h_y_12@163.com>
2016-05-03 17:19:04 +0800

    添加mongo的配置文件

---
+++ src/main/java/com/freemarker/platform/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package com.freemarker.platform.dao.impl;
+
+
+import com.freemarker.platform.dao.UserDao;
+import com.freemarker.platform.entity.User;
+import com.mongo.orm.dao.impl.AbstractRepository;
+
+public class UserDaoImpl extends AbstractRepository<User> implements UserDao{
+	
+}
---
+++ src/test/java/com/freemarker/platform/dao/UserTest.java
@@ -0,0 +1,38 @@
+package com.freemarker.platform.dao;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.freemarker.platform.dao.impl.UserDaoImpl;
+import com.freemarker.platform.entity.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class UserTest extends AbstractJUnit4SpringContextTests {
+	private static Log log = LogFactory.getLog(UserTest.class.getName());
+
+	private UserDao userDao;
+
+	@Test
+	public void init() {
+		log.debug("开始启动");
+		userDao = new UserDaoImpl();
+		System.out.println(userDao);
+		User user = new User();
+		user.setId("1");
+		user.setUsername("zhangsan");
+		user.setUsername("123456");
+		userDao.insert(user);
+	}
+
+//	public void insert() {
+//		User p = new User();
+//		userDao.insert(p);
+//		log.debug("添加成功");
+//	}
+}
